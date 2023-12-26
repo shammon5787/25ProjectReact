4e6d4956bad77eea3b57da369be27270c7caeb2e
Sadam <sadam.bsit@gmail.com>
2023-12-26 19:09:59 +0500

    new change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-
 const Navbar = () => {
+
 
     const items = useSelector((state)=>state.cart);
 
@@ -15,6 +15,7 @@ const Navbar = () => {
         <span className='cartCount'>
             Cart Items: {items.length}
         </span>
+
       </div>
     </div>
   )
